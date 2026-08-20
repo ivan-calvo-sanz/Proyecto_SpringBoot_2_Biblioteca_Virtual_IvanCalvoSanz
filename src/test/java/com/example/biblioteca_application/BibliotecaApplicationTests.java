377bbdfad51a839963624c1b622cdd74f349a7ae
ivan-calvo-sanz <ivan.calvo.sanz@gmail.com>
2026-08-20 11:39:55 +0200

    260820 version-01

---
+++ src/test/java/com/example/biblioteca_application/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biblioteca_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
